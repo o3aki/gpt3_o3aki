5c07b9c00bd9a7ab05f8f463f911e5838a46bb03
Arthur D <o3akue@gmail.com>
2023-11-08 22:00:32 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Article from './components/article/Article';
+import Brand from './components/brand/Brand';
+import CTA from './components/cta/CTA';
+import Feature from './components/feature/Feature';
+import Navbar from './components/navbar/Navbar';
+
+const App = () => {
+  return (
+    <div>
+      <h1>GPT-3</h1>
+    </div>
+  )
+}
+
+export default App
